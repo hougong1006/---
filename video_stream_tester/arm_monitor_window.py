b4f9bce8df58735533400899c72044bd821fb00f
侯工 <2105242280@qq.com>
2026-09-09 18:48:20 +0800

    fix: update monitor default device IP

--- video_stream_tester/arm_monitor_window.py
+++ video_stream_tester/arm_monitor_window.py
@@ -577,10 +577,10 @@ class ArmMonitorWindow(QMainWindow):
 
         # Load saved IP or use default
         self._settings = QSettings('DofbotPro', 'ArmMonitor')
-        saved_ip = self._settings.value('last_ip', '10.182.135.172')
+        saved_ip = self._settings.value('last_ip', '192.168.0.104')
         if saved_ip in ('10.229.42.172', '10.178.59.172',
                         '10.182.135.194', '192.168.8.88'):
-            saved_ip = '10.182.135.172'
+            saved_ip = '192.168.0.104'
             self._settings.setValue('last_ip', saved_ip)
         self.ip_input = QLineEdit(saved_ip)
         self.ip_input.setFont(QFont("Consolas", 9))
